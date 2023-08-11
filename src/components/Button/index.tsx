dc533169c2ac34f65632fe5a68ae10cee7730f1a
Kowit Karunas <kowit2dev@gmail.com>
2023-08-11 17:27:12 -0400

    Initial commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable react/button-has-type */
+import React, { ElementType, FunctionComponent, ReactNode, forwardRef } from 'react'
+import clsx from 'clsx'
+import Loading from '../Loading'
+
+import styles from './button.module.scss'
+
+export interface Props {
+  /** Different style types */
+  btnType?: 'primary' | 'primary-blue' | 'link' | 'secondary' | 'tertiary'
+  children?: ReactNode
+  /** Overrides wrapper DOM element */
+  component?: ElementType
+  /** Overrides styles */
+  className?: string
+  /** Link to leave page. Turns the button into an "a" tag. */
+  href?: string
+  /** Adds a loading icon and disables the button. Use for submitting forms. */
+  loading?: boolean
+  /** color of the loader */
+  loadingColor?: string
+  /** Size of button */
+  size?: 'lg' | 'md' | 'sm'
+  /** The default type to be applied to the button */
+  type?: 'button' | 'submit' | 'reset' | ''
+  [x: string]: unknown // ...rest property
+}
+
+const marginRight = { marginRight: '15px' }
+
+const Button: FunctionComponent<Props> = forwardRef<unknown, Props>(function ButtonComp(props, ref) {
+  const {
+    btnType = 'primary',
+    children = null,
+    className = '',
+    href = '',
+    loading = false,
+    loadingColor = '#fff',
+    size = 'lg',
+    type = 'button',
+    component: Component = 'button',
+    ...rest
+  } = props
+  const load = loading ? styles.loading : ''
+  const btnClassName = clsx(styles.btn, styles[size], load, styles[btnType], className)
+
+  const buttonChildren = (
+    <div className={styles['btn-flex']}>
+      {loading && <Loading color={loadingColor} style={marginRight} />}
+      {children}
+    </div>
+  )
+
+  if (href) {
+    return (
+      // @ts-ignore
+      <a href={href} className={btnClassName} ref={ref} {...rest}>
+        {buttonChildren}
+      </a>
+    )
+  }
+
+  return (
+    <Component type={type} className={btnClassName} ref={ref} {...rest}>
+      {buttonChildren}
+    </Component>
+  )
+})
+
+export default Button
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,52 @@
+import React, { memo } from 'react'
+import Spinner from '../Icons/Spinner'
+
+import styles from './loading.module.scss'
+
+export interface Props {
+  /** Override styles on wrapper */
+  className?: string
+  /** RBA or HEX value of spinner */
+  color?: string
+  /** Size of height/width of spinner */
+  size?: 'sm' | 'md' | 'lg' | string
+  /** Different types of spinners */
+  type?: 'ring' | 'dots' // Pass "ring" for loading circle or "dots" for loading dots
+  [x: string]: unknown // ...rest property
+}
+
+const sizes = {
+  sm: '25px',
+  md: '34px',
+  lg: '44px'
+}
+
+const Loading: React.FunctionComponent<Props> = ({
+  className = '',
+  color = '#d63426',
+  size = 'md',
+  type = 'ring',
+  ...rest
+}) => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore
+  const loadingSize: string = sizes[size] || size // can be 'sm', 'md', 'lg' or a pixel size
+  const dotStyle = {
+    color,
+    fontSize: `${loadingSize}`
+  }
+  const ringClassNameWrapper = `${styles['loading-ring']} ${className}`.trim()
+  const dotsClassNameWrapper = `${styles['loading-dots']} ${className}`.trim()
+
+  if (type === 'dots') {
+    return <div className={dotsClassNameWrapper} style={dotStyle} {...rest} />
+  }
+
+  return (
+    <div className={ringClassNameWrapper} {...rest}>
+      <Spinner strokeColor={color} width={loadingSize} height={loadingSize} data-testid="spinner" />
+    </div>
+  )
+}
+
+export default memo(Loading)
